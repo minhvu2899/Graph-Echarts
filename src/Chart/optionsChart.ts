5f6f07219ec1b6cb1dec86ec65eac5f2658ec399
thudta1 <thudta@rikkeisoft.com>
2022-10-14 18:14:36 +0700

    add chart

--- src/Chart/optionsChart.ts
+++ src/Chart/optionsChart.ts
@@ -2,6 +2,12 @@ import { ReactEChartsProps } from "../components/ReactECharts";
 import type { SeriesOption } from "echarts";
 import * as echarts from "echarts/core";
 import { validateData } from "../utils/validataData";
+import {
+  CallbackDataParams,
+  TooltipFormatterCallback,
+  TooltipOption,
+  TopLevelFormatterParams,
+} from "echarts/types/dist/shared";
 
 export const getOptions = (
   dataC: string[],
@@ -95,7 +101,15 @@ export const getOptions = (
     seriesDemo.push({
       name: "Bar1",
       type: "bar",
-      data: validateData(dataLength, dataBar1),
+      data: [
+        [-0.1, 30],
+        [0.9, 30],
+        [1.9, 50],
+        [2.9, 40],
+        [3.9, 60],
+        [4.9, 70],
+        [5.9, 80],
+      ],
       barWidth: "2",
       barMaxWidth: 1,
       itemStyle: {
@@ -113,13 +127,40 @@ export const getOptions = (
       },
       zlevel: 10,
     });
+    seriesDemo.push({
+      name: "s2",
+      type: "scatter",
+      z: 10,
+      data: [
+        [-0.1, 30],
+        [0.9, 30],
+        [1.9, 50],
+        [2.9, 40],
+        [3.9, 60],
+        [4.9, 70],
+        [5.9, 80],
+      ],
+      itemStyle: {
+        color: "white",
+        borderColor: "green",
+      },
+    });
     legend.push("Bar1");
   }
   if (dataBar2) {
+    console.log(validateData(dataLength, dataBar2, 2));
     seriesDemo.push({
       name: "Bar2",
       type: "bar",
-      data: validateData(dataLength, dataBar2),
+      data: [
+        [0.1, 30],
+        [1.1, 30],
+        [2.1, 50],
+        [3.1, 40],
+        [4.1, 60],
+        [5.1, 70],
+        [6.1, 80],
+      ],
       barWidth: "2",
       barGap: "10",
       itemStyle: {
@@ -132,6 +173,23 @@ export const getOptions = (
       stack: "x",
     });
     legend.push("Bar2");
+    seriesDemo.push({
+      name: "s2",
+      type: "scatter",
+      data: [
+        [0.1, 30],
+        [1.1, 30],
+        [2.1, 50],
+        [3.1, 40],
+        [4.1, 60],
+        [5.1, 70],
+        [6.1, 80],
+      ],
+      itemStyle: {
+        color: "white",
+        borderColor: "blue",
+      },
+    });
   }
 
   return {
@@ -142,17 +200,36 @@ export const getOptions = (
     //   containLabel: true,
     // },
     tooltip: {
-      trigger: "axis",
+      trigger: "item",
       axisPointer: { type: "shadow" },
+      formatter: function (params: CallbackDataParams | CallbackDataParams[]) {
+        console.log(typeof params, params);
+        // if (!Array.isArray(params)) {
+        //   console.log(params?.marker);
+        //   return params?.marker;
+        // }
+        // params?.map((item) => {
+        //   return (
+        //     '<span style="display:inline-block;margin-right:4px;border-radius:10px;width:10px;height:10px;background-color:#4D8AE3;"></span>' +
+        //     item
+        //   );
+        // });
+        return "asdasdasd";
+      },
     },
     title: {
       text: "ECharts Demo",
     },
     xAxis: {
-      type: "category",
+      type: "value",
       name: "Exam",
       boundaryGap: true,
-      data: dataC,
+      data: [1, 2, 3, 4, 5, 6, 7, 8, 9],
+      show: true,
+      axisLabel: {
+        formatter: "Exam {value}",
+        align: "center",
+      },
     },
     yAxis: [
       {
@@ -185,32 +262,150 @@ export const getOptions = (
       icon: "rect",
     },
     series: [
-      ...seriesDemo,
-      //
-
-      // {
-      //   type: "bar",
-      //   stack: "x",
-      //   data: [2, 2, 2, 2, 2, 2, 2, 2, 2],
-      //   roundCap: true,
-      // },
       {
-        type: "scatter",
-        data: [5, 8, 7, 20, 22, 50, 40, 12, 20],
+        name: "2021",
+        data: validateData(dataLength, dataS),
+        type: "line",
+        //Size dot
+        //   symbolSize: 10,
+        symbol: "none",
+        //Color line
         itemStyle: {
-          color: "white",
-          borderColor: "green",
+          color: "#80AAA8",
+          // opacity: 0.2,
+        },
+        //Size line
+        lineStyle: {
+          width: 4,
+        },
+        // markLine: {
+        //   symbol: ["none", "none"],
+        //   symbolOffset: ["none", "none"],
+        //   label: { show: false },
+        //   data: xAxis,
+        // },
+        areaStyle: {
+          color: new echarts.graphic.LinearGradient(1, 0, 1, 1, [
+            {
+              offset: 0,
+              color: "#78ACA4",
+            },
+            {
+              offset: 1,
+              color: "#D2DBDA",
+            },
+          ]),
+          // opacity: 0.3,
         },
       },
       {
-        type: "scatter",
-        data: [2, 4, 7, 23, 25, 76, 50, 12, 20],
+        name: "Bar1",
+
+        data: [
+          [0, 30],
+          [1, 30],
+          [2, 50],
+          [3, 40],
+          [4, 60],
+          [5, 70],
+          [6, 80],
+        ],
+        type: "bar",
+        z: 10,
+        barWidth: "1",
         itemStyle: {
+          color: "#80AAA8",
+          borderWidth: 1,
+          borderType: "dashed",
+        },
+      },
+      {
+        itemStyle: {
+          borderColor: "#80AAA8",
           color: "white",
-          borderColor: "green",
+          borderWidth: 3,
+        },
+        data: [
+          [0, 30],
+          [1, 30],
+          [2, 50],
+          [3, 40],
+          [4, 60],
+          [5, 70],
+          [6, 80],
+        ],
+        type: "scatter",
+        z: 10,
+        tooltip: {
+          trigger: "none",
+        },
+      },
+      {
+        itemStyle: {
+          color: "#4D8AE3",
+          borderWidth: 1,
+          borderType: "dashed",
+        },
+        name: "Bar2",
+        data: [
+          [0.3, 30],
+          [1.3, 30],
+          [2.3, 50],
+          [3.3, 40],
+          [4.3, 60],
+          [5.3, 70],
+          [6.3, 80],
+        ],
+        type: "bar",
+        z: 10,
+        barWidth: "1",
+        tooltip: {
+          trigger: "axis",
+        },
+      },
+      {
+        itemStyle: {
+          borderColor: "#4D8AE3",
+          color: "white",
+          borderWidth: 3,
+          // opacity: 0.2,
+        },
+        data: [
+          [0.3, 30],
+          [1.3, 30],
+          [2.3, 50],
+          [3.3, 40],
+          [4.3, 60],
+          [5.3, 70],
+          [6.3, 80],
+        ],
+        type: "scatter",
+        z: 10,
+        tooltip: {
+          trigger: "none",
         },
       },
     ],
+    // series: [
+    //   ...seriesDemo,
+    //   //
+
+    //   // {
+    //   //   type: "bar",
+    //   //   stack: "x",
+    //   //   data: [2, 2, 2, 2, 2, 2, 2, 2, 2],
+    //   //   roundCap: true,
+    //   // },
+
+    //   {
+    //     type: "scatter",
+    //     data: [2, 4, 7, 23, 25, 76, 50, 12, 20],
+    //     itemStyle: {
+    //       color: "white",
+    //       borderColor: "green",
+    //     },
+    //   },
+    // ],
   };
 };
 // export const getOptions1: ReactEChartsProps["option"] = {
--- src/utils/validataData.ts
+++ src/utils/validataData.ts
@@ -1,5 +1,14 @@
-export const validateData = (length: number, data: any[]) => {
-  if (data && data.length === length) return data;
-  if (data.length > length) return data.slice(0, length);
-  return data.concat(new Array(length - data.length).fill(0));
+export const validateData = (length: number, data: any[], stt = 1) => {
+  let newData = [];
+  if (data && data.length === length) newData = data;
+  if (data.length > length) newData = data.slice(0, length);
+  newData = data.concat(new Array(length - data.length).fill(0));
+  const arr = newData.map((item, idx) => {
+    if (stt === 1) {
+      return [idx, item];
+    }
+    return [idx + 0.3, item];
+  });
+  console.log(arr);
+  return arr;
 };
